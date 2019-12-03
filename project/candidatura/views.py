5e615c80dc8898e71f48ccf78421cdb98ee72b09
luizlgam <lgamjoeperry@gmail.com>
2019-12-03 02:02:55 -0300

    Import http methods

--- project/candidatura/views.py
+++ project/candidatura/views.py
@@ -6,6 +6,8 @@ from candidato.models import Candidato
 from vaga.models import Vaga
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 
